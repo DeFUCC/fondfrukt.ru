4d11311bc67330e74e3765fbc78e10acdd02103d
davay42 <fond@fondfrukt.ru>
2021-07-15 00:22:30 +0300

    dep

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:chromatone/chromatone.center.git master:gh-pages
+git push -f git@github.com:DeFUCC/frkt.ru.git master:gh-pages
 
 cd -
