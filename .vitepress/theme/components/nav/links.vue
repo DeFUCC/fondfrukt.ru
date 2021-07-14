7ee1ae9948b89287ab051996287f1bec8bcce7a1
davay42 <fond@fondfrukt.ru>
2021-07-14 23:37:53 +0300

    first working site

---
+++ .vitepress/theme/components/nav/links.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+a.p-2.no-underline.transition-all.duration-300(
+  v-for="page in site.customData.pages.main"
+  :key= "page.link"
+  :href="page.link"
+  :class="{ active: route.path.includes(page.link) }"
+) {{ page.title }} 
+</template>
+
+<script setup>
+import { useData, useRoute } from 'vitepress'
+const { site, frontmatter } = useData();
+
+const route = useRoute();
+</script>
+  
+<style scoped>
+.active {
+  @apply bg-light-900/50 rounded;
+}
+</style>
