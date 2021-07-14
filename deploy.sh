db5a7be9af5abf5f765c2e54cc6bc3d3db1f41fb
davay42 <fond@fondfrukt.ru>
2021-07-15 00:30:08 +0300

    dep

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:DeFUCC/frkt.ru.git master:gh-pages
+git push -f git@github.com:DeFUCC/DeFUCC.github.io.git master:master
 
 cd -
