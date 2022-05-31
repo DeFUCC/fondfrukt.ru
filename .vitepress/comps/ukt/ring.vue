e0b41991277c6fc644ad9b87037dde30e5b66e78
davay <fond@fondfrukt.ru>
2022-05-31 21:59:25 +0300

    ukt

--- .vitepress/comps/ukt/ring.vue
+++ .vitepress/comps/ukt/ring.vue
@@ -1,6 +1,4 @@
 <script setup>
-
-
 const props = defineProps({
   size: { type: Number, default: 1000 },
   pad: { type: Number, default: 50 },
@@ -9,27 +7,25 @@ const props = defineProps({
 
 const emit = defineEmits(['update:modelValue'])
 
-function getCircleCoord(n = 0, total = 12, radius = 35, width = props.size) {
-  let angle = ((n - total / 4) / (total / 2)) * Math.PI // Calculate the angle at which the element will be placed.
-  // For a semicircle, we would use (i / numNodes) * Math.PI.
-  let x = radius * Math.cos(angle) + width / 2 // Calculate the x position of the element.
-  let y = radius * Math.sin(angle) + width / 2 // Calculate the y position of the element.
-  return { x, y }
+const letters = {
+  y: 'У',
+  k: 'К',
+  t: 'Т'
 }
 
 const params = {
   k: [0, 1, 3, 4],
-  u: [1, 5, 7],
+  y: [1, 5, 7],
   t: [2, 4, 6]
 }
 
 const colors = {
   k: '#ed1c27',
-  u: '#f7941d',
+  y: '#f7941d',
   t: '#00a651'
 }
 
-const stroke = 50
+const stroke = 60
 
 function polarToCartesian(r, degrees) {
   const radians = degrees * Math.PI / 180.0;
@@ -52,27 +48,35 @@ function segmentPath(r, d0, d1) {
   ].join('')
 }
 
+const rank = computed(() => `У${Math.floor(props.modelValue.y)} К${Math.floor(props.modelValue.k)} T${Math.floor(props.modelValue.t)}`)
+const average = computed(() => (Math.floor(props.modelValue.y) + Math.floor(props.modelValue.k) + Math.floor(props.modelValue.t)) / 3)
+
 </script>
 
 <template lang='pug'>
 svg.max-w-150.mx-auto(
   version="1.1",
   baseProfile="full",
-  :viewBox="`${-pad - size / 2} ${-pad - size / 2} ${size + 2 * pad} ${size + 2 * pad} `",
+  :viewBox="`${-pad - size / 2} ${-pad - size / 2} ${size + 2 * pad} ${size + 4 * pad} `",
   xmlns="http://www.w3.org/2000/svg",
   text-anchor="middle" 
   dominant-baseline="middle" 
-
   )
+  circle(r="500" fill="#0001" stroke-width="2" stroke="#5553") 
   circle(r="50" fill="#5553")
-  circle(r="500" fill="none" stroke-width="2" stroke="#5553") 
-  //- text {{ modelValue }}
+  text.text-90px.select-none.font-bold 
+    tspan(y="10" text-anchor="middle"   dominant-baseline="middle" ) {{ average.toFixed() }}
+  g.text-120px.select-none.font-bold(:transform="`translate(0, ${size / 2 + pad + 20})`")
+    rect(:width="size - 2 * pad" :height="2 * pad + 30" :x="-size / 2 + pad" :y="-pad - 20" rx="30" fill="#8882")
+    g(v-for="(color, c, i) in colors" :key="color" :transform="`translate(${-250 + i * 250},8)`" )
+      text {{ letters[c] }}{{ Math.floor(modelValue[c]) }}
   g(v-for="(sectors, p) in params" :key="p")
     g(v-for="sector in sectors" :key="sector" :transform="`rotate(${sector * 45 - 90})`")
       g(v-for="layer in 5" :key="layer" stroke-linecap="round" :stroke="colors[p]" :stroke-width="stroke" )
-        path(
-          fill="none" 
-          v-if="modelValue[p] >= layer"
-          :d="segmentPath(layer * 80 + 50, -22.5 + (8 - layer) * 2, 22.5 - (8 - layer) * 2)"
-          )
+        transition(name="fade")
+          path(
+            fill="none" 
+            v-if="modelValue[p] >= layer"
+            :d="segmentPath(layer * 80 + 30, -22.5 + (7 - layer) * 3, 22.5 - (7 - layer) * 3)"
+            )
 </template>
---
+++ .vitepress/comps/ukt/row.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  param: { type: String, default: 'u' }
+})
+
+const rate = computed(() => {
+  let r = { u: props.param == 'u' ? 5 : 0, k: 2, t: 4 }
+  return r
+})
+
+</script>
+
+<template lang='pug'>
+.flex.flex-wrap
+  ukt-ring.w-30(v-for="n in 5" :key="n" v-model="rate")
+</template>
